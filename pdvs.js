cbb50624d9301559f39cfc58374e4ff1fd36131a
Jonas Pereira <129891766+jpereira55@users.noreply.github.com>
2023-10-22 16:56:16 -0300

    Create pdvs.js

---
+++ pdvs.js
@@ -0,0 +1,101 @@
+let pdvsJson = [ 
+    {
+        name: 'PDV 1',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.101:9898/normal.html'
+    },
+    {
+        name: 'PDV 2',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.102:9898/normal.html'
+    },
+    {
+        name: 'PDV 3',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.103:9898/normal.html'
+    },
+    {
+        name: 'PDV 4',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.104:9898/normal.html'
+    },
+    {
+        name: 'PDV 5',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.105:9898/normal.html'
+    },
+    {
+        name: 'PDV 6',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.106:9898/normal.html'
+    },
+    {
+        name: 'PDV 7',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.107:9898/normal.html'
+    },
+    {
+        name: 'PDV 8',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.108:9898/normal.html'
+    },
+    {
+        name: 'PDV 9',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.109:9898/normal.html'
+    },
+    {
+        name: 'PDV 10',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.110:9898/normal.html'
+    },
+    {
+        name: 'PDV 11',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.111:9898/normal.html'
+    },
+    {
+        name: 'PDV 12',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.112:9898/normal.html'
+    },
+    {
+        name: 'PDV 13',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.113:9898/normal.html'
+    },
+    {
+        name: 'PDV 14',
+        descripion:'Padaria',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.114:9898/normal.html'
+    },
+    {
+        name: 'PDV 15',
+        descripion:'Reserva',
+        img: 'imagens/1.jpg',
+        link: 'http://172.23.87.115:9898/normal.html'
+    },
+    {
+        name: 'PDV 16',
+        descripion:'Frente de Loja',
+        img: 'imagens/1.jpg',
+        link: 'http://192.168.60.190:9898/normal.html'
+    },
+    
+    
+    
+];
