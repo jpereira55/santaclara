afe97541dc98655d8667bd0f5a1a461199fc2a0b
Jonas Pereira <129891766+jpereira55@users.noreply.github.com>
2023-10-22 17:09:35 -0300

    Update app.js

--- app.js
+++ app.js
@@ -11,34 +11,18 @@ pdvsJson.map((pdv) => {
     pdvsclone.querySelector('.card p').innerHTML = pdv.descripion;
 
 
-    if(pdv.descripion === 'Rotisseria') {
-      pdvsclone.querySelector('.card').style.background = '#000000';
     }
-    if(pdv.descripion === 'A la Carte') {
-      pdvsclone.querySelector('.card').style.background = '#708090';
-    }
-    if(pdv.descripion === 'Churros') {
-      pdvsclone.querySelector('.card').style.background = '#00008B';
-    }
-    if(pdv.descripion === 'Adega') {
+    if(pdv.descripion === 'Preferencial') {
       pdvsclone.querySelector('.card').style.background = '#8B0000';
-    }
-    if(pdv.descripion === 'Boteco') {
-      pdvsclone.querySelector('.card').style.background = '#002D58';
     }
     if(pdv.descripion === 'Super Drive') {
       pdvsclone.querySelector('.card').style.background = '#004b23';
     }
     if(pdv.descripion === 'Padaria') {
-      pdvsclone.querySelector('.card').style.background = '#B22222';
-    }
-    if(pdv.descripion === 'ELETRO') {
       pdvsclone.querySelector('.card').style.background = '#D83006';
     }
     if(pdv.descripion === 'RESERVA') {
       pdvsclone.querySelector('.card').style.background = '#D83006';
     }
-  if(pdv.descripion === 'Area Nova') {
-      pdvsclone.querySelector('.card').style.background = '#A9A9A9';
-    }
+ 
 });
